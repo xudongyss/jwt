e4cb09732e6f72ef5c8da14024b39729fdf356ca
xudongyss <xudong.yss@gmail.com>
2021-01-26 16:25:27 +0800

    Update RefreshToken.php

--- src/RefreshToken.php
+++ src/RefreshToken.php
@@ -5,11 +5,13 @@
 namespace XuDongYss\JWT;
 
 use Defuse\Crypto\Crypto;
+use Defuse\Crypto\Exception\EnvironmentIsBrokenException;
 
 class RefreshToken{
 	protected $encryptionKey;
 	protected $identifier = '';
 	protected $expires = '+1 month';
+	protected $refreshToken = [];
 	
 	/**
 	 * 初始化
@@ -17,27 +19,76 @@ class RefreshToken{
 	 * @param string 	$expires
 	 */
 	public function __construct($encryptionKey, $expires = '') {
+		$this->encryptionKey = $encryptionKey;
 		if($expires) $this->expires = $expires;
 	}
 	
+	/**
+	 * 创建
+	 * @param int 		$uid
+	 * @param string 	$accessTokenId
+	 * @return string
+	 */
 	public function create($uid, $accessTokenId) {
-		$this->identifier = $this->generateUniqueIdentifier();
-		$_data = [
-			'refresh_token_id'=> $this->identifier,
+		$this->refreshToken = [
+			'refresh_token_id'=> $this->generateUniqueIdentifier(),
 			'access_token_id'=> $accessTokenId,
 			'uid'=> $uid,
 			'expire_time'=> strtotime($this->expires),
 		];
-		$plaintext = json_encode($_data);
+		$refreshTokenJson = json_encode($this->refreshToken);
 		
-		return Crypto::encryptWithPassword($plaintext, $this->encryptionKey);
+		try{
+			return Crypto::encryptWithPassword($refreshTokenJson, $this->encryptionKey);
+		}catch(EnvironmentIsBrokenException $e) {
+			throw new \Exception('RefreshToken 生成失败');
+		}catch(\TypeError $e) {
+			throw new \Exception('RefreshToken 生成失败');
+		}
+	}
+	
+	/**
+	 * 校验
+	 * @param string 		$refreshTokenString
+	 * @throws \Exception
+	 */
+	public function validating($refreshTokenString) {
+		try{
+			$refreshTokenJson = Crypto::decryptWithPassword($refreshTokenString, $this->encryptionKey);
+			$refreshToken = json_decode($refreshTokenJson, true);
+			if($refreshToken) {
+				$this->refreshToken = $refreshToken;
+				$this->validExpireTime();
+				
+				return $this->refreshToken;
+			}
+			
+			throw new \Exception('RefreshToken 错误');
+		}catch(EnvironmentIsBrokenException $e) {
+			throw new \Exception('RefreshToken 错误');
+		}catch(\TypeError $e) {
+			throw new \Exception('RefreshToken 错误');
+		}catch(\Exception $e) {
+			throw new \Exception($e->getMessage());
+		}
+	}
+	
+	public function getClaim($key) {
+		return $this->refreshToken[$key];
+	}
+	
+	/**
+	 * 是否过期
+	 * @throws \Exception
+	 * @return boolean
+	 */
+	protected function validExpireTime() {
+		if($this->refreshToken['expire_time'] < time()) throw new \Exception('RefreshToken 已过期');
+		
+		return true;
 	}
 	
 	protected function generateUniqueIdentifier($length = 40) {
 		return \bin2hex(\random_bytes($length));
-	}
-	
-	public function getIdentifier() {
-		return $this->identifier;
 	}
 }
